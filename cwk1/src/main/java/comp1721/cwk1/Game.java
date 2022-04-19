53488dd2de56911f96eaf048d2ba8bfd7e00a1a2
Junzhe Wang <sc20j2w@leeds.ac.uk>
2022-04-19 18:30:40 +0800

    Initial files for Coursework 1

---
+++ cwk1/src/main/java/comp1721/cwk1/Game.java
@@ -0,0 +1,12 @@
+package comp1721.cwk1;
+
+
+public class Game {
+  // TODO: Implement constructor with String parameter
+
+  // TODO: Implement constructor with int and String parameters
+
+  // TODO: Implement play() method
+
+  // TODO: Implement save() method, with a String parameter
+}
---
+++ cwk1/src/main/java/comp1721/cwk1/WordList.java
@@ -0,0 +1,10 @@
+package comp1721.cwk1;
+
+
+public class WordList {
+  // TODO: Implement constructor with a String parameter
+
+  // TODO: Implement size() method, returning an int
+
+  // TODO: Implement getWord() with an int parameter, returning a String
+}
